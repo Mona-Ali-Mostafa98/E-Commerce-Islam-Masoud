290dfc1061f2066f07c901324eb4f11d42f223a2
Mona Ali Mostafa <monaali980628@gmail.com>
2022-10-04 13:28:46 +0200

    Add all routes of admin

---
+++ routes/admin.php
@@ -0,0 +1,71 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\BlogController;
+use App\Http\Controllers\Admin\ContactController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\GalleryController;
+use App\Http\Controllers\Admin\OfferController;
+use App\Http\Controllers\Admin\OrderController;
+use App\Http\Controllers\Admin\PartnerController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\RoleController;
+use App\Http\Controllers\Admin\ServiceController;
+use App\Http\Controllers\Admin\SettingController;
+use App\Http\Controllers\Admin\SliderController;
+use App\Http\Controllers\Admin\SubscribeController;
+use App\Http\Controllers\Admin\UserController;
+use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+Route::group(
+[
+	'prefix' => LaravelLocalization::setLocale(),
+	'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ] //to open website with last used lang
+], function(){
+
+    Route::prefix('admin')->name('admin.')->group(function () {
+        Route::get('login', [AuthController::class, 'login'])->name('login')->middleware('guest:admin');
+        Route::post('/do_login', [AuthController::class, 'do_login'])->name('do_login')->middleware('guest:admin');
+
+        Route::middleware('isAdmin:admin')->group(function(){
+
+            Route::get('dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+            Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+
+            Route::resource('roles', RoleController::class);
+
+            Route::resource('admins', AdminController::class);
+
+            Route::get('/settings/edit', [SettingController ::class, 'edit'])->name('settings.edit');
+            Route::put('/settings/update/{setting}', [SettingController::class, 'update'])->name('settings.update');
+
+            Route::resource('offers', OfferController::class);
+            Route::resource('partners', PartnerController::class);
+            Route::resource('services', ServiceController::class);
+
+
+            Route::resource('sliders', SliderController::class);
+
+            Route::resource('blogs', BlogController::class);
+
+
+            Route::resource('products', ProductController::class);
+
+            Route::resource('galleries', GalleryController::class);
+
+
+            Route::resource('users', UserController::class);
+
+            Route::resource('orders', OrderController::class)->except('create' , 'store');
+
+
+            Route::resource('contacts', ContactController::class)->only('index' , 'show' , 'destroy');
+
+            Route::resource('subscribes', SubscribeController::class)->only('index' , 'destroy');
+
+        });
+
+    });
+});
